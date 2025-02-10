606c354a73af0548721155c9946b2690cee866ea
LiamAs05 <liamdrop205@gmail.com>
2025-02-10 21:38:18 +0200

    Sorted into files

---
+++ src/medial_axis.lua
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------
+-- Medial Axis Ipelet
+----------------------------------------------------------------------
+
+label = "Medial Axis"
+
+about = [[
+Computes the Medial Axis of a polygon.
+]]
+
+ipelet = false
+
+function run(model, num)
+  -- if not ipelet then ipelet = assert(ipe.Ipelet(dllname)) end
+  -- model:runIpelet(methods[num].label, ipelet, num)
+  model:warning("WIP - DLL is a WIP")
+end
+
