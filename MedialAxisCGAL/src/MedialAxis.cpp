a3eb1432b28a1933d95892ba05e2f1be7bc21a21
liamd <liamd@LIAMPC>
2025-08-02 21:51:04 +0300

    Changed to C++20, refactored code

--- MedialAxisCGAL/src/MedialAxis.cpp
+++ MedialAxisCGAL/src/MedialAxis.cpp
@@ -1,36 +1,43 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polygon_2.h>
+#include <CGAL/Boolean_set_operations_2.h>
 #include <CGAL/IO/WKT.h>
 #include <iostream>
 #include <algorithm>
+//#include <CGAL/CGAL_Ipelet_base.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_2 Point;
 typedef CGAL::Polygon_2<K> Polygon_2;
+typedef CGAL::Polygon_with_holes_2<K> Polygon_with_holes_2;
 typedef K::Line_2 Line;
+typedef K::Segment_2 Segment;
 
 using std::cout;
 using std::endl;
 
-Polygon_2 polygon_from_wkt()
+class MedialAxis
 {
-    const std::string coordinates_file_path = R"(C:\Users\liamd\Desktop\ipe-7.2.29\bin\coordinates.txt)";
-    std::ifstream wkt_file(coordinates_file_path);
+public:
+    explicit MedialAxis(const Polygon_2& pgn);
+    std::list<Segment> getMedialAxis() const;
 
-    if (!wkt_file) {
-        throw std::runtime_error("Could not open WKT file: " + coordinates_file_path);
-    }
+private:
+    static std::pair<Segment, Segment> findNextSegmentPair(std::vector<CGAL::Point_2<CGAL::Epick>> vertices,
+        std::pair<Line, Line>& meeting_edges, Point& center);
 
-    Polygon_2 poly;
-    if (!read_polygon_WKT(wkt_file, poly)) {
-        throw std::runtime_error("Failed to parse WKT polygon.");
-    }
+	void addMedialAxisSegments(Polygon_2 poly, const std::pair<Segment, Segment>& earliest_meeting_pair);
 
-    return poly;
-}
+	static void updateVertices(std::vector<CGAL::Point_2<CGAL::Epick>>& vertices, const std::pair<Segment, Segment>& earliest_meeting_pair,
+        const std::pair<Line, Line>& meeting_edges);
+
+	void triangleMedialAxis(const std::vector<CGAL::Point_2<CGAL::Epick>>& vertices);
+
+	std::list<Segment> m_medialAxisSegments;
+};
 
 std::optional<Point> get_intersection_point(const Line& l1, const Line& l2) {
-    const CGAL::Object obj = CGAL::intersection(l1, l2);
+    const CGAL::Object obj = intersection(l1, l2);
     if (const Point* p = CGAL::object_cast<Point>(&obj)) {
         return *p;
     }
@@ -55,65 +62,107 @@ Line get_angle_bisector(const Point& prev, const Point& curr, const Point& next)
     return { curr, curr + bisector };
 }
 
-std::vector<std::pair<Point, Point>> compute_medial_axis(const Polygon_2& poly) {
-    std::vector<std::pair<Point, Point>> result;
-    std::vector vertices(poly.vertices_begin(), poly.vertices_end());
+std::pair<Segment, Segment> MedialAxis::findNextSegmentPair(std::vector<CGAL::Point_2<CGAL::Epick>> vertices, std::pair<Line, Line>& meeting_edges, Point& center)
+{
+    std::pair<Segment, Segment> earliest_meeting_pair;
+    double min_radius = std::numeric_limits<double>::max();
     std::size_t n = vertices.size();
 
-    if (n < 3)
+	for (std::uint64_t i = 0; i < n; ++i) {
+		const std::uint64_t i0 = (i - 1 + n) % n;
+		const std::uint64_t i1 = i;
+		const std::uint64_t i2 = (i + 1) % n;
+		const std::uint64_t i3 = (i + 2) % n;
+
+		Line bisector1 = get_angle_bisector(vertices[i0], vertices[i1], vertices[i2]);
+		Line bisector2 = get_angle_bisector(vertices[i1], vertices[i2], vertices[i3]);
+
+		auto inter = get_intersection_point(bisector1, bisector2);
+		if (!inter)
+		{
+			continue;
+		}
+
+		const double r = std::sqrt(CGAL::squared_distance(*inter, vertices[i1]));
+		if (r < min_radius) {
+			min_radius = r;
+			meeting_edges = { Line(vertices[i0], vertices[i1]), Line(vertices[i2], vertices[i3]) };
+			center = *inter;
+            earliest_meeting_pair = { {vertices[i1], center}, {vertices[i2], center} };
+		}
+	}
+
+    return earliest_meeting_pair;
+}
+
+void MedialAxis::addMedialAxisSegments(Polygon_2 poly, const std::pair<Segment, Segment>& earliest_meeting_pair)
+{
+    m_medialAxisSegments.emplace_back(earliest_meeting_pair.first);
+    m_medialAxisSegments.emplace_back(earliest_meeting_pair.second);
+}
+
+void MedialAxis::updateVertices(std::vector<CGAL::Point_2<CGAL::Epick>>& vertices, const std::pair<Segment, Segment>& earliest_meeting_pair, const std::pair<Line, Line>& meeting_edges)
+{
+	vertices.push_back(get_intersection_point(meeting_edges.first, meeting_edges.second).value());
+	vertices.erase(std::ranges::find(vertices, earliest_meeting_pair.first.point(0)));
+	vertices.erase(std::ranges::find(vertices, earliest_meeting_pair.second.point(0)));
+}
+
+void MedialAxis::triangleMedialAxis(const std::vector<CGAL::Point_2<CGAL::Epick>>& vertices)
+{
+	const Point A = vertices[0], B = vertices[1], C = vertices[2];
+	Point center = CGAL::centroid(A, B, C);
+	m_medialAxisSegments.emplace_back(A, center);
+	m_medialAxisSegments.emplace_back(B, center);
+	m_medialAxisSegments.emplace_back(C, center);
+}
+
+MedialAxis::MedialAxis(const Polygon_2& pgn)
+{
+    if (!pgn.is_convex() || !pgn.is_simple()) {
+        throw std::runtime_error("This algorithm only supports simple convex polygons.");
+    }
+
+    std::vector vertices(pgn.vertices_begin(), pgn.vertices_end());
+
+    if (vertices.size() < 3)
     {
         throw std::runtime_error("Not enough vertices in chosen polygon.");
     }
 
-    while (n > 3) {
-        double min_radius = std::numeric_limits<double>::max();
-        std::pair<Point, Point> earliest_meeting_pair;
+    while (vertices.size() > 3) {
         std::pair<Line, Line> meeting_edges;
         Point center;
 
-        for (std::uint64_t i = 0; i < n; ++i) {
-            const std::uint64_t i0 = (i - 1 + n) % n;
-            const std::uint64_t i1 = i;
-            const std::uint64_t i2 = (i + 1) % n;
-            const std::uint64_t i3 = (i + 2) % n;
-
-            Line bisector1 = get_angle_bisector(vertices[i0], vertices[i1], vertices[i2]);
-            Line bisector2 = get_angle_bisector(vertices[i1], vertices[i2], vertices[i3]);
-
-            auto inter = get_intersection_point(bisector1, bisector2);
-            if (!inter)
-            {
-                continue;
-            }
-
-            const double r = std::sqrt(CGAL::squared_distance(*inter, vertices[i1]));
-            if (r < min_radius) {
-                min_radius = r;
-                earliest_meeting_pair = { vertices[i1], vertices[i2] };
-                meeting_edges = { Line(vertices[i0], vertices[i1]), Line(vertices[i2], vertices[i3]) };
-                center = *inter;
-            }
-        }
-
-        result.emplace_back(earliest_meeting_pair.first, center);
-        result.emplace_back(earliest_meeting_pair.second, center);
-        vertices.push_back(get_intersection_point(meeting_edges.first, meeting_edges.second).value());
-
-        vertices.erase(std::ranges::find(vertices, earliest_meeting_pair.first));
-        auto second_idx = std::ranges::find(vertices, earliest_meeting_pair.second);
-        vertices.erase(second_idx);
-
-        n = vertices.size();
+        auto next_meeting_bisectors= findNextSegmentPair(vertices, meeting_edges, center);
+        addMedialAxisSegments(Polygon_2(vertices.begin(), vertices.end()), next_meeting_bisectors);
+        updateVertices(vertices, next_meeting_bisectors, meeting_edges);
     }
 
     // Final triangle case
-    const Point A = vertices[0], B = vertices[1], C = vertices[2];
-    Point center = CGAL::centroid(A, B, C);
-    result.emplace_back(A, center);
-    result.emplace_back(B, center);
-    result.emplace_back(C, center);
+    triangleMedialAxis(vertices);
+}
 
-    return result;
+std::list<Segment> MedialAxis::getMedialAxis() const
+{
+    return m_medialAxisSegments;
+}
+
+Polygon_2 polygon_from_wkt()
+{
+    const std::string coordinates_file_path = R"(C:\Users\liamd\Desktop\ipe-7.2.29\bin\coordinates.txt)";
+    std::ifstream wkt_file(coordinates_file_path);
+
+    if (!wkt_file) {
+        throw std::runtime_error("Could not open WKT file: " + coordinates_file_path);
+    }
+
+    Polygon_2 poly;
+    if (!read_polygon_WKT(wkt_file, poly)) {
+        throw std::runtime_error("Failed to parse WKT polygon.");
+    }
+
+    return poly;
 }
 
 int main()
@@ -121,19 +170,13 @@ int main()
     try {
         const Polygon_2 pgn = polygon_from_wkt();
 
-        cout << "The polygon is " << (pgn.is_simple() ? "" : "not ") << "simple." << endl;
-        cout << "The polygon is " << (pgn.is_convex() ? "" : "not ") << "convex." << endl;
-
-        if (!pgn.is_convex() || !pgn.is_simple()) {
-            throw std::runtime_error("This algorithm only supports simple convex polygons.");
-        }
-
-        const auto segments = compute_medial_axis(pgn);
+        MedialAxis m(pgn);
+        const auto segments = m.getMedialAxis();
         cout << "Medial Axis Edges: " << segments.size() << endl;
 
-        for (const auto& [fst, snd] : segments) {
-            cout << fst.x() << " " << fst.y() << " -> "
-                << snd.x() << " " << snd.y() << endl;
+        for (const auto& seg : segments) {
+            cout << seg.point(0) << " -> "
+                << seg.point(1) << endl;
         }
     }
     catch (const std::exception& ex) {
@@ -143,3 +186,53 @@ int main()
 
     return 0;
 }
+
+//
+//namespace medial_axis_ipelet
+//{
+//    const std::string labels[] = { "Medial Axis", "Help" };
+//
+//    const std::string hmsg[] = {
+//          "Compute the Medial Axis transformation of a simple convex polygon."
+//    };
+//
+//    class MedialAxisIpelet : public CGAL::Ipelet_base<K, 2>
+//    {
+//    public:
+//        MedialAxisIpelet() : Ipelet_base("Medial Axis", labels, hmsg) {}
+//        void protected_run(int) override;
+//    };
+//
+//    void MedialAxisIpelet::protected_run(int fn)
+//    {
+//        switch (fn) {
+//        case 1:
+//            show_help();//print an help message
+//            return;
+//        default:
+//            std::list<Point_2> pt_lst;
+//
+//            Recovering points using output iterator of type
+//            Dispatch_or_drop_output_iterator
+//            read_active_objects(
+//                CGAL::dispatch_or_drop_output<Point_2>(std::back_inserter(pt_lst))
+//            );
+//
+//            if (pt_lst.empty()) {
+//                print_error_message("No mark selected");
+//                return;
+//            }
+//
+//            const Polygon_2 pgn = Polygon_2(pt_lst.begin(), pt_lst.end());
+//            MedialAxis m(pgn);
+//
+//            for (const auto& seg : m.getMedialAxis())
+//            {
+//                draw_in_ipe(seg);
+//            }
+//        };
+//    }
+//
+//}
+//
+//CGAL_IPELET(medial_axis_ipelet::MedialAxisIpelet)
